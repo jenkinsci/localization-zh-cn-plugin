7c78cf83b0493903d8016535b42227c373633097
Daniel Beck <daniel-beck@users.noreply.github.com>
2019-02-05 22:39:10 +0100

    [JEP-216] Add support for webapp resources

--- src/main/java/io/jenkins/plugins/localization_zh_cn/LocalizationContributorImpl.java
+++ src/main/java/io/jenkins/plugins/localization_zh_cn/LocalizationContributorImpl.java
@@ -1,6 +1,7 @@
 package io.jenkins.plugins.localization_zh_cn;
 
 import hudson.Extension;
+import hudson.PluginWrapper;
 import io.jenkins.plugins.localization.support.LocalizationContributor;
 
 import javax.annotation.CheckForNull;
@@ -13,5 +14,11 @@ public class LocalizationContributorImpl extends LocalizationContributor {
     @Override
     public URL getResource(@Nonnull String s) {
         return getClass().getClassLoader().getResource(s);
+    }
+
+    @CheckForNull
+    @Override
+    public URL getPluginResource(@Nonnull String s, @Nonnull PluginWrapper pluginWrapper) {
+        return getClass().getClassLoader().getResource("/webapp/" + s);
     }
 }
