e6d3f6a9aebe2a1bc248d50a2bce94d7dcb31cc0
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-03-18 09:29:51 +0800

    remove unused files

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mvn clean package -DskipTests
-curl -u admin:11a2c2a54f98e8eae81786d304f6c08c9c http://jenkins.k8s.surenpi.com/pluginManager/uploadPlugin -F "name=@target/localization-zh-cn.hpi"
-curl -u admin:11a2c2a54f98e8eae81786d304f6c08c9c http://jenkins.k8s.surenpi.com/restart -X POST
