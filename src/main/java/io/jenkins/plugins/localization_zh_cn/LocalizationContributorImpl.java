52b1245aeed6db2e0146b1470075939aee8529b3
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2019-05-02 12:43:49 +0200

    [JEP-216] Extract localization support infrastructure into localization-support plugin (#31)
    * Use new localization-support plugin
    
    * Update with new core contributed resources
    
    * Use localization-support timestamped snapshot
    
    * Timestamped snapshots
    
    * Make NewVersionAvailable work based on new third argument from core
    
    * Migrate webapp/ help files
    
    * Add dummy zn_CN localization html files for credentials plugin
    
    * [JEP-216] Add support for webapp resources
    
    * Make webapp location explicit matching LocalizationContributorImpl
    
    * MUST REVERT: Use dummy localization of credentials-plugin
    
    * Fix pom
    
    * Rename file to match actual resource name
    
    * move credentials-plugin into plugins dir
    
    * Use Jenkins 2.173 and incrementals deployment of localization-support
    
    * Remove duplicate help files
    
    * Fix credentials plugin resource location
    
    * Integrate jenkinsci/localization-support-plugin#1
    
    * Use webapp subdirectory in resources
    
    * Remove utility localizations for use during development only
    
    * Depend on released version of localization-support:1.1
    
    * Remove more temporary localization files
    
    * Back to original resource location

---
+++ src/main/java/io/jenkins/plugins/localization_zh_cn/LocalizationContributorImpl.java
@@ -0,0 +1,24 @@
+package io.jenkins.plugins.localization_zh_cn;
+
+import hudson.Extension;
+import hudson.PluginWrapper;
+import io.jenkins.plugins.localization.support.LocalizationContributor;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import java.net.URL;
+
+@Extension
+public class LocalizationContributorImpl extends LocalizationContributor {
+    @CheckForNull
+    @Override
+    public URL getResource(@Nonnull String s) {
+        return getClass().getClassLoader().getResource(s);
+    }
+
+    @CheckForNull
+    @Override
+    public URL getPluginResource(@Nonnull String s, @Nonnull PluginWrapper pluginWrapper) {
+        return getClass().getClassLoader().getResource("/webapp/" + s);
+    }
+}
