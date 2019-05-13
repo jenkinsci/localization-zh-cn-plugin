70ad17951c5d813d64cb17742b8dee08f6bc7c44
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-05-13 22:11:44 +0800

    Show the community link only current language is zh_CN

--- src/main/java/io/jenkins/plugins/localization_zh_cn/CommunityDecorator.java
+++ src/main/java/io/jenkins/plugins/localization_zh_cn/CommunityDecorator.java
@@ -2,13 +2,24 @@ package io.jenkins.plugins.localization_zh_cn;
 
 import hudson.Extension;
 import hudson.model.PageDecorator;
-import hudson.model.UnprotectedRootAction;
-import org.jenkinsci.Symbol;
-import org.kohsuke.stapler.StaplerProxy;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerRequest;
 
-import javax.annotation.CheckForNull;
-import javax.annotation.Nonnull;
+import java.util.Locale;
 
 @Extension
 public class CommunityDecorator extends PageDecorator {
+    public boolean isCurrentLanguage() {
+        StaplerRequest req = Stapler.getCurrentRequest();
+        if(req == null) {
+            return false;
+        }
+
+        String languages = req.getHeader("Accept-Language");
+        if(languages != null) {
+            return languages.startsWith(Locale.SIMPLIFIED_CHINESE.toString());
+        }
+
+        return false;
+    }
 }
--- src/test/java/io/jenkins/plugins/localization_zh_cn/CommunityPageTest.java
+++ src/test/java/io/jenkins/plugins/localization_zh_cn/CommunityPageTest.java
@@ -1,11 +1,20 @@
 package io.jenkins.plugins.localization_zh_cn;
 
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.UrlUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+
+import static org.junit.Assert.*;
 
 public class CommunityPageTest {
     @Rule
@@ -14,6 +23,32 @@ public class CommunityPageTest {
     @Test
     public void indexPage() throws IOException, SAXException {
         JenkinsRule.WebClient client = rule.createWebClient();
-        client.goTo("/chinese");
+
+        client.goTo("chinese");
+    }
+
+    @Test
+    public void locale() throws IOException, SAXException {
+        JenkinsRule.WebClient client = rule.createWebClient();
+        URL rootURL = UrlUtils.toUrlUnsafe(client.getContextPath());
+
+        WebRequest webRequest = new WebRequest(rootURL);
+        webRequest.setAdditionalHeader("Accept-Language", Locale.ENGLISH.toString());
+
+        // for English locale
+        HtmlPage page = client.getPage(webRequest);
+        HtmlAnchor anchor = null;
+        try {
+            page.getAnchorByText("Jenkins 中文社区");
+        } catch (ElementNotFoundException e) {
+            // should throw an exception here
+        }
+
+        // for Chinese locale
+        webRequest.removeAdditionalHeader("Accept-Language");
+        webRequest.setAdditionalHeader("Accept-Language", Locale.SIMPLIFIED_CHINESE.toString());
+        page = client.getPage(webRequest);
+        anchor = page.getAnchorByText("Jenkins 中文社区");
+        assertNotNull(anchor);
     }
 }
